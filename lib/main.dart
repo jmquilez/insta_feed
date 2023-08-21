4d17ef0242e1fb354903c9faf696a5bd4276ea36
jmquilez <jmquilez>
2023-08-21 17:10:37 +0200

    jank proof

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //TODO, CHECK: INTERLEAVED FRAMES? (30 FPS VIDEO, CHANGE), SEVERAL CONSECUTIVE JANKY FRAMES, MEMORY USAGE??
     //TODO: SmoothParent??
     return /*SmoothParent(
       child: */
